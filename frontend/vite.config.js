a95708c8a2ea217ecbd4829a0f0e07a6df4c145b
Johannes Weber <johannes@drweber.de>
2025-04-12 14:03:42 -0700

    Updated vite config allower hosts section

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3030,
-  }
-})
+    plugins: [react()],
+    server: {
+        port: 3030,
+        host: true,
+        allowedHosts: [
+            "server.wnet.wn",
+            "localhost",
+        ],
+    },
+});
