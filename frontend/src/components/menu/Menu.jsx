1c119fb3165b4498661680ab9c494a015912c2c7
Johannes Weber <johannes@drweber.de>
2025-04-12 15:58:01 -0700

    Support marking items as unavailable and hiding.

--- frontend/src/components/menu/Menu.jsx
+++ frontend/src/components/menu/Menu.jsx
@@ -6,6 +6,40 @@ function Menu() {
     const [menu, setMenu] = useState([]);
     const [config, setConfig] = useState([]);
     const baseUrl = `${constants.apiRoot}/records/`;
+    const pushEndpoint = `${constants.apiRoot}/${constants.apiPushEndpoint}`;
+
+    // Use Dynforms M2M to update the record in the database.
+    function updateDb(record) {
+        fetch(pushEndpoint, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                collectionName: constants.menuCollectionName,
+                record,
+                clientId: constants.clientId
+            }),
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Database update failed.");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                console.log("Update successful.");
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    }
+
+    // Update a property on an item on the record.
+    function setItemProperty(record, itemName, propertyName, value) {
+        const itemIndex = record.items.findIndex((item) => item.name === itemName);
+        record.items[itemIndex][propertyName] = value;
+    }
 
     useEffect(() => {
         // Fetch the configuration.
@@ -13,7 +47,7 @@ function Menu() {
             fetch(`${baseUrl}${constants.configCollectionName}`)
                 .then((response) => {
                     if (!response.ok) {
-                        throw new Error("Bad response from dynforms server (fetching configuration).");
+                        throw new Error("Bad response from dynforms.");
                     }
                     return response.json();
                 })
@@ -22,28 +56,25 @@ function Menu() {
                         throw new Error("Bad configuration returned from server.");
                     }
 
-                    console.log("Successfully fetched the configuration.", data.records[0]);
-
                     setConfig(data.records[0]);
 
                     // Fetch the menu.
                     fetch(`${baseUrl}${constants.menuCollectionName}`)
                         .then((response) => {
                             if (!response.ok) {
-                                throw new Error("Bad response from dynforms server (fetching menu).");
+                                throw new Error("Bad response from dynforms.");
                             }
                             return response.json();
                         })
                         .then((data) => {
-                            console.log(`Successfully fetched the menu. (${data.records.length} groups)`);
                             setMenu(data.records);
                         })
                         .catch((err) => {
-                            console.log("Error", err);
+                            console.error("Error fetching menu:", err);
                         });
                 })
                 .catch((err) => {
-                    console.log("Error", err);
+                    console.error("Error fetching configuration:", err);
                 });
         }
 
@@ -56,8 +87,6 @@ function Menu() {
     }, [])
  
     const groupsToShow = menu.filter(itemGroup => itemGroup.show_group);
-
-    console.log('Showing the following groups:', groupsToShow.map(group => group.group_name).join(', '));
 
     const menuStyle = {
         backgroundImage: `url(${config.background_image})`,
@@ -77,6 +106,7 @@ function Menu() {
         fontWeight: "bold",
         textShadow: "0.05em 0.05em 0.1em rgba(0, 0, 0, 0.4)",
     };
+
     return (
         <div className="menu-container" style={menuStyle}>
             <div className="menu-top-header" style={topHeaderStyle}>
@@ -85,14 +115,15 @@ function Menu() {
 
             {groupsToShow.map((itemGroup, index) => {
                 if (!itemGroup.font_size) {
-                    itemGroup.font_size = 30;
+                    // Default
+                    itemGroup.font_size = 40;
                 }
                 const itemGroupStyle = {
                     position: "absolute",
                     left: itemGroup.x + "px",
                     top: itemGroup.y + "px",
                     width: itemGroup.width + "px",
-                    //height: itemGroup.height + "px",
+                    height: itemGroup.height + "px",
                     fontSize: itemGroup.font_size + "px",
                     border: itemGroup.border,
                     padding: 25,
@@ -106,25 +137,68 @@ function Menu() {
                     textDecoration: "underline",
                 };
 
-                const itemsToShow = itemGroup.items.filter((item) => item.quantity > 0).sort((a, b) => a.name > b.name ? 1 : -1);
-                console.log('items', itemsToShow)
+                // Toggle availability on single click.
+                function itemClick(e) {
+                    const itemName = e.currentTarget.dataset.name;
+                    const recordId = e.currentTarget.dataset.record_id;
 
+                    // The attribute stores the boolean as a string, "true" or "false".
+                    const availableString = e.currentTarget.dataset.available;
+                    const available = availableString !== "false";
+                    const availableToggled = !available;
+
+                    // Update the UI.
+                    e.target.style.textDecoration = availableToggled ? "none" : "line-through";
+                    e.target.style.opacity = availableToggled ? "100%" : "60%";
+                    e.currentTarget.dataset.available = availableToggled ? "true" : "false";
+
+                    // Update the database.
+                    const record = menu.find(record => record._id === recordId);
+                    setItemProperty(record, itemName, "available", !available);
+                    updateDb(record);
+                }
+
+                // Hide the item on double click.
+                function itemDoubleClick(e) {
+                    const itemName = e.currentTarget.dataset.name;
+                    const recordId = e.currentTarget.dataset.record_id;
+
+                    // Update the UI (hide the item).
+                    e.currentTarget.style.display = "none";
+
+                    // Update the database.
+                    const record = menu.find((record) => record._id === recordId);
+                    setItemProperty(record, itemName, "hidden", true);
+                    updateDb(record);
+                }
+
+                // Render the menu.
+                const itemsToShow = itemGroup.items.filter((item) => !item.hide).sort((a, b) => a.name > b.name ? 1 : -1);
                 if (itemsToShow.length) {
+                    // Have menu items to show.
                     return (
                         <div key={index} style={itemGroupStyle}>
                             <div style={groupNameStyle}>{itemGroup.group_name}</div>
 
                             {itemsToShow.map((item, index) => {
-                                return <div key={index}>{item.name}</div>;
+                                const itemStyle = {};
+                                itemStyle.textDecoration = item.available ? "none" : "line-through";
+                                itemStyle.opacity = item.available ? "100%" : "60%",
+                                itemStyle.display = item.hidden ? "none" : "block";
+
+                                return <div key={index} data-name={item.name} data-record_id={itemGroup._id} data-available={item.available} style={itemStyle} onClick={itemClick} onDoubleClick={itemDoubleClick}>{item.name}</div>;
                             })}
                         </div>
                     );
                 } else {
+                    // No item group has any unhidden content.
                     return <></>;
                 }
             })}
         </div>
     );
 }
+
+
 
 export default Menu
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,8 @@
 // Constants
 const constants = {
     apiRoot: "http://server.wnet.wn:3010/db",
+    apiPushEndpoint: "m2m/push",
+    clientId: "barmenu",
     menuCollectionName: "barmenu",
     configCollectionName: "barmenu_config",
 };
